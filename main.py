4002a553b7d81ce0cbc6c0fc4a82039159fcc2c4
Ian <ianmacbeth1@gmail.com>
2022-06-24 23:15:00 -0400

    night time and landings

--- main.py
+++ main.py
@@ -10,19 +10,18 @@ loc = astral.LocationInfo(name='SFZ', timezone='America/New_York', latitude=41.9
 s = sun(loc.observer, date=START_DATE, tzinfo=loc.timezone)
 
 
-with open('Table_5293.csv', 'r', newline='') as csvfile:
+with open('Table_8893.csv', 'r', newline='') as csvfile:
     data = csv.DictReader(csvfile)
     with open('Updater.csv', 'w', newline='') as updater:
         updater.writelines('ForeFlight Logbook Import\n\nFlights Table\n')
         good = csv.DictWriter(updater, fieldnames=['Date', 'AircraftID', 'From', 'To', 'Route', 'TotalTime', 'PIC', 'Night', 'CrossCountry','DayTakeoffs', 'DayLandingsFullStop', 'NightTakeoffs', 'NightLandingsFullStop', 'AllLandings', 'DualGiven', 'Person1'])
         good.writeheader()
         for i in data:
-            d = i['ï»¿"Flight Date"'].split()[0].split('/')
-            t = i['ï»¿"Flight Date"'].split()[1].split(':')
+            d = i['Start'].split()[0].split('/')
+            t = i['Start'].split()[1].split(':')
             #HH:MM:SS
-            c = i['ï»¿"Flight Date"'].split()[2]
+            c = i['Start'].split()[2]
             #AM/PM
-            #Date = datetime.date(int(d[2]), int(d[0]), int(d[1]))
             h = int(t[0])
             if c == 'PM' and h < 12: h += 12
             DateTime = datetime.datetime(int(d[2]), int(d[0]), int(d[1]), h, int(t[1]), int(t[2]), tzinfo=loc.tzinfo)
@@ -32,11 +31,17 @@ with open('Table_5293.csv', 'r', newline='') as csvfile:
                 AircraftID = i['Aircraft']
                 if AircraftID != "":
                     AircraftID = AircraftID.split()[0]
-                if i['Activity Type'] == 'Intro Flight': Route = 'HHIGH VC401'
+                if i['Activity Type'] == 'Intro Flight': Route = 'VC401'
                 else: Route = ''
-                Time = i['Total Flight Instruction']
+                Time = i['Total Flight Time']
                 Night = 0
+                #FSP GIVES END TIME FOR SOME REASON
+                DayLandings = 1
+                NightLandings = 0
                 if DateTime > s['dusk']:
                     Night = Time
+                    DayLandings = 0
+                    NightLandings = 1
+                Landings = DayLandings + NightLandings
                 Person = ' '.join(i['Customer 1 Name'].split()[:2]) + ';Student;'
-                good.writerow({'Date': DateTime.date(), 'AircraftID': AircraftID, 'From': 'KSFZ', 'To': 'KSFZ', 'Route': Route, 'TotalTime': Time, 'PIC': Time, 'Night':Night, 'DayTakeoffs': 1, 'DayLandingsFullStop': 1, 'AllLandings': 1, 'DualGiven': Time, 'Person1': Person})
+                good.writerow({'Date': DateTime.date(), 'AircraftID': AircraftID, 'From': 'KSFZ', 'To': 'KSFZ', 'Route': Route, 'TotalTime': Time, 'PIC': Time, 'Night':Night, 'DayTakeoffs': DayLandings, 'DayLandingsFullStop': DayLandings, 'NightTakeoffs': NightLandings, 'NightLandingsFullStop': NightLandings, 'AllLandings': Landings, 'DualGiven': Time, 'Person1': Person})
